51226e114cb654d2776c82dfe71605477593644b
heg78 <heg78@ymail.ru>
2024-06-08 15:56:46 +0300

    feat: comments

---
+++ main-service/src/main/java/ru/practicum/dto/comment/CountCommentsByEventDto.java
@@ -0,0 +1,16 @@
+package ru.practicum.dto.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CountCommentsByEventDto {
+
+    private Long eventId;
+    private Long countComments;
+}
